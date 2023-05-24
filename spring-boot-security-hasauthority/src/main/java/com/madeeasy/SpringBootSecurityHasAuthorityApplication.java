ff0c2f76c8c2a28e3d8befdd68abba07282eb848
madeeasycodinglife <126568738+madeeasycodinglife@users.noreply.github.com>
2023-05-24 11:00:18 +0530

    Add files via upload

---
+++ spring-boot-security-hasauthority/src/main/java/com/madeeasy/SpringBootSecurityHasAuthorityApplication.java
@@ -0,0 +1,13 @@
+package com.madeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityHasAuthorityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityHasAuthorityApplication.class, args);
+	}
+
+}
---
+++ spring-boot-security-hasauthority/src/main/java/com/madeeasy/entity/Roles.java
@@ -0,0 +1,5 @@
+package com.madeeasy.entity;
+
+public enum Roles {
+    AUTHORITY_ADMIN, AUTHORITY_MANAGER, AUTHORITY_USER;
+}
